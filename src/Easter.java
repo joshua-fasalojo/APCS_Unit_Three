dd5f546ed0e9970d2ccc5ec291a5073a201d1c76
joshua-fasalojo <fasalif@ssfs.org>
2021-10-29 09:56:56 -0400

    Compound expressions

---
+++ src/Easter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Easter {
+
+
+    public static String dateOfEaster() {
+
+   // }
+
+    public static Boolean isValidYear(int year) {
+        int Year = 1982-2048;
+
+
+    //}
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the year?");
+        int newYear = scan.nextInt();
+
+    }
+}
--- src/LoneTeen.java
+++ src/LoneTeen.java
@@ -1,11 +1,13 @@
-//public class LoneTeen {
+public class LoneTeen {
 
-    // In the space below, write the loneTeen method. Make sure the signature matches
-    // the one given in the instructions.
-    //public static boolean loneTeen(int num1, int num2) {
-        //zif (num1 >= 13 && num1 <=19);
+    //In the space below, write the loneTeen method. Make sure the signature matches
+    //the one given in the instructions.
+    public static void loneTeen(int num1, int num2) {
+        if (num1 >= 13 && num1 <= 19)
+        {
+        }
 
 
 
- //}
-//
+    }
+}
